13484c90eb1760a352d57483a399a4bc53162031
dev2bang <bmwf14@gmail.com>
2017-12-05 23:23:02 +0900

    just tutorial using scikit learn score in kaggle : 71.7

---
+++ titanic/titanic.py
@@ -0,0 +1,48 @@
+# from www.datacamp.com
+# open course (kaggle-python-on-machine-learning)
+
+import pandas as pd
+import numpy as np
+from sklearn import tree
+
+# Load the train, test dataset from CSV (www.kaggle.com)
+train_file = "train.csv"
+test_file = "test.csv"
+train = pd.read_csv(train_file)
+test = pd.read_csv(test_file)
+
+# Cleaning and Formatting Data
+train.loc[train["Sex"] == "male", "Sex"] = 0
+train.loc[train["Sex"] == "female", "Sex"] = 1
+train["Age"] = train["Age"].fillna(train["Age"].median())
+train["Embarked"] = train["Embarked"].fillna("S")
+train.loc[train["Embarked"] == "S", "Embarked"] = 0
+train.loc[train["Embarked"] == "C", "Embarked"] = 1
+train.loc[train["Embarked"] == "Q", "Embarked"] = 2
+
+test.loc[test["Sex"] == "male", "Sex"] = 0
+test.loc[test["Sex"] == "female", "Sex"] = 1
+train["Age"] = train["Age"].fillna(train["Age"].median())
+test["Embarked"] = test["Embarked"].fillna("S")
+test.loc[test["Embarked"] == "S", "Embarked"] = 0
+test.loc[test["Embarked"] == "C", "Embarked"] = 1
+test.loc[test["Embarked"] == "Q", "Embarked"] = 2
+
+# Select and create features for decision tree
+target = train["Survived"].values
+train_features = train[["Pclass", "Sex", "Age", "Fare"]].values
+
+# Create and fit decision tree
+my_tree_one = tree.DecisionTreeClassifier()
+my_tree_one = my_tree_one.fit(train_features, target)
+
+# Predict result using test DataSet
+test.loc[152, "Fare"] = test["Fare"].median()
+test["Age"] = test["Age"].fillna(test["Age"].median())
+test_features = test[["Pclass", "Sex", "Age", "Fare"]].values
+
+my_prediction = my_tree_one.predict(test_features)
+PassengerId = np.array(test["PassengerId"]).astype(int)
+predict_result = pd.DataFrame(my_prediction, PassengerId, columns = ["Survived"])
+
+predict_result.to_csv("result.csv", index_label = ["PassengerId"])
