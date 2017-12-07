f4b10c67821024dd629ffbc46105989bf8fa9b64
dev2bang <bmwf14@gmail.com>
2017-12-07 12:17:32 +0900

    add features

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -15,34 +15,30 @@ test = pd.read_csv(test_file)
 train.loc[train["Sex"] == "male", "Sex"] = 0
 train.loc[train["Sex"] == "female", "Sex"] = 1
 train["Age"] = train["Age"].fillna(train["Age"].median())
-train["Embarked"] = train["Embarked"].fillna("S")
-train.loc[train["Embarked"] == "S", "Embarked"] = 0
-train.loc[train["Embarked"] == "C", "Embarked"] = 1
-train.loc[train["Embarked"] == "Q", "Embarked"] = 2
+train["FamilySize"] = train["SibSp"] + train["Parch"] + 1
 
 test.loc[test["Sex"] == "male", "Sex"] = 0
 test.loc[test["Sex"] == "female", "Sex"] = 1
 train["Age"] = train["Age"].fillna(train["Age"].median())
-test["Embarked"] = test["Embarked"].fillna("S")
-test.loc[test["Embarked"] == "S", "Embarked"] = 0
-test.loc[test["Embarked"] == "C", "Embarked"] = 1
-test.loc[test["Embarked"] == "Q", "Embarked"] = 2
+test["FamilySize"] = test["SibSp"] + test["Parch"] + 1
 
 # Select and create features for decision tree
 target = train["Survived"].values
-train_features = train[["Pclass", "Sex", "Age", "Fare"]].values
+train_features = train[["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "FamilySize"]].values
 
 # Create and fit decision tree
-my_tree_one = tree.DecisionTreeClassifier()
+my_tree_one = tree.DecisionTreeClassifier();
 my_tree_one = my_tree_one.fit(train_features, target)
 
 # Predict result using test DataSet
 test.loc[152, "Fare"] = test["Fare"].median()
 test["Age"] = test["Age"].fillna(test["Age"].median())
-test_features = test[["Pclass", "Sex", "Age", "Fare"]].values
+test_features = test[["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "FamilySize"]].values
 
 my_prediction = my_tree_one.predict(test_features)
 PassengerId = np.array(test["PassengerId"]).astype(int)
 predict_result = pd.DataFrame(my_prediction, PassengerId, columns = ["Survived"])
+
+print(my_tree_one.score(train_features, target))
 
 predict_result.to_csv("result.csv", index_label = ["PassengerId"])
